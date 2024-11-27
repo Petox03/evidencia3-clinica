569f78204d714e6dd6e66d89cde4493ec918c77c
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2024-11-27 04:19:00 -0600

    chore(proyect): se creó el proyecto con filament y se hicieron migraciones y modelos

---
+++ database/migrations/2024_11_27_101207_create_treatments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Medicine;
+use App\Models\Pet;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Pet::class);
+            $table->foreignIdFor(Medicine::class);
+            $table->string('amount');
+            $table->time('administration_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treatments');
+    }
+};
