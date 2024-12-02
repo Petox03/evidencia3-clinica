68e602366819770f86efa18399b82ba8470e716b
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2024-12-02 15:01:15 -0600

    feat(roles): se añadieron roles y permisos, se crearon seeders para que el usuario admin tenga rol admin y se creó la política para que el recurso de los usuarios sólo pueda ser manipulado por admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +21,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@filamentphp.com',
             'password' => '$2y$12$p9Krjec/15cAth90GisgVe3uHCucUuYWrxNmN74dc8/xO1dtlxoii'
         ]);
+
+        DB::table('roles')->insert([ 'name' => 'admin', 'guard_name' => 'web', 'created_at' => now(), 'updated_at' => now(), ]);
+        DB::table('roles')->insert([ 'name' => 'empleado', 'guard_name' => 'web', 'created_at' => now(), 'updated_at' => now(), ]);
+        DB::table('roles')->insert([ 'name' => 'recepcionista', 'guard_name' => 'web', 'created_at' => now(), 'updated_at' => now(), ]);
+
+        DB::table('model_has_roles')->insert([ 'role_id' => 1, 'model_type' => 'App\Models\User', 'model_id' => 1, ]);
 
         $this->call([PetTypeSeeder::class, CustomerSeeder::class, PetSeeder::class, MedicineSeeder::class, TreatmentSeeder::class,]);
     }
